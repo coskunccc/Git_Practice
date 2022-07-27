52f89affc70aae22cf5333180c567cb2e3d1967a
coskunccc <coskunarslan@yahoo.com>
2022-07-27 13:31:16 +0300

    Java study file now commiting

--- src/GitStudy.java
+++ src/GitStudy.java
@@ -1,4 +1,15 @@
 public class GitStudy {
+
+
+    public void met(){
+
+        System.out.println("super clasta");
+    }
+
+    public void metYeni(){
+        System.out.println("yeni");
+    }
+    public static int a=9;
     public static void main(String[] args) {
 
     }
---
+++ src/GitStudy1.java
@@ -0,0 +1,14 @@
+public class GitStudy1 extends GitStudy{
+    public void met(){
+        System.out.println("subclasda");
+    }
+    public void oyle(){
+        System.out.println("oyle iste");
+    }
+    int a=5;
+
+    public static void main(String[] args) {
+        GitStudy obj=new GitStudy1();
+       
+    }
+}
